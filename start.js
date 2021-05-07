8c7530b42420cdfb2eae520ed2be0b39cacf7100
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-07 01:27:40 -0400

    Update start.js

--- start.js
+++ start.js
@@ -11,15 +11,15 @@ mongoose.connect(uri, {
 
 mongoose.connection
   .on('open', () => {
-    alert('Mongoose connection open');
+    console.log('Mongoose connection open');
   })
   .on('error', (err) => {
-    alert(`Connection error: ${err.message}`);
+    console.log(`Connection error: ${err.message}`);
   });
 
 require('./models/Registration');
 const app = require('./app');
 
 const server = app.listen(PORT, () => {
-  alert(`Express is running on port ${server.address().port}`);
+  console.log(`Express is running on port ${server.address().port}`);
 });
