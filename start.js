9b247c95ffecabd70302e20fadac994c41f49b4b
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-07 01:58:27 -0400

    Update start.js

--- start.js
+++ start.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,    
-
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 mongoose.connection
