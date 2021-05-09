0db1374f5b2a8bd180b48aa06adf3ba2a9e9b79a
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-09 17:12:55 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,9 @@ router.post('/',
     if (errors.isEmpty()) {
       const registration = new Registration(req.body);
       registration.save()
-       .then(() => { res.render('index', { title: 'Listing Registrations' }); })
+      // .then(() => { res.render('index', { title: 'Listing Registrations' }); })
       // .then(() => { res.send('Thank you for your registration!'); })
-      //  .then(() => { res.render('form', { title: 'Registration form' }); })
+        .then(() => { res.render('form', { title: 'Registration form' }); })
       //  .then(() => { res.send(form); })
       //  .then(() => { res.redirect('/'); })
       //  .then(() => { console.log('Your information has been registered '); })
