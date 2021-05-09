0e897b813c167e606fda288816a5cf024fc2b9bd
adambaughn <77374380+adambaughn@users.noreply.github.com>
2021-05-09 02:24:01 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.post('/',
       const registration = new Registration(req.body);
       registration.save()
       //  .then(() => { res.send('Your information has been registered.'); })
-        .then(() => { res.redirect('/'); })
+        .then(() => { res.redirect('form'); })
       //  .then(() => { console.log('Your information has been registered '); })
         .catch((err) => {
           console.log(err);
